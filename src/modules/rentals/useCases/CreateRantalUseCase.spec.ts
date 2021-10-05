7a690c021d77d76ea85b1e2bd5bf1e30d0f4c210
Roberto <jrggroberto@gmail.com>
2021-10-05 19:37:04 -0300

    ch: created first rentals tests

---
+++ src/modules/rentals/useCases/CreateRantalUseCase.spec.ts
@@ -0,0 +1,22 @@
+import { RentalsRepositoryInMemory } from "../repositories/in-memory/RentalsRepositoryInMemory";
+import { CreateRentalUseCase } from "./CreateRentalUsecase";
+
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe("Create Rental", () =>{
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it("should be able to create a new rental", async() =>{
+    await createRentalUseCase.execute({
+      user_id: "12345",
+      car_id: "1222212",
+      expected_return_date: new Date(),
+    
+    });
+  });
+})
